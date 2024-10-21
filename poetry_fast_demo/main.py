f4804f5dcb192a3b8b994644525862d429bf4e67
chencai <chencai@xsyxsc.com>
2024-10-21 21:40:09 +0800

    refractor main.py

--- poetry_fast_demo/main.py
+++ poetry_fast_demo/main.py
@@ -1,13 +1,1 @@
-import os
-import logging.config
-
-import yaml
-
-log_conf_path = os.environ.get("LOG_CONFIG")
-with open(log_conf_path, 'r', encoding="utf-8") as f:
-    dict_conf = yaml.safe_load(f)
-logging.config.dictConfig(dict_conf)
-
-from module1.calc import do_something
-
-do_something()
+print("hello world")
